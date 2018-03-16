732651f3d4c4ba5fa014c1708933ddf2d809d476
Santos Gallegos <santos_g@outlook.com>
2018-03-16 00:03:54 -0500

    Ejemplos

---
+++ examples/python/README.md
@@ -0,0 +1,102 @@
+# Spotify.nvim
+
+Control Spotify from Neovim.
+
+![1](https://user-images.githubusercontent.com/4975310/36114436-055b913e-0ffe-11e8-93ef-37dc9f487852.png)
+![2](https://user-images.githubusercontent.com/4975310/36114434-04f6c1e6-0ffe-11e8-9232-eda34f5e0e65.png)
+![3](https://user-images.githubusercontent.com/4975310/36114432-04d2de0c-0ffe-11e8-97f0-9b1fb287a20b.png)
+![4](https://user-images.githubusercontent.com/4975310/36114431-04a225dc-0ffe-11e8-9085-edc6e60438fe.png)
+![5](https://user-images.githubusercontent.com/4975310/36114430-04811716-0ffe-11e8-8291-4a8bb03a466c.png)
+![6](https://user-images.githubusercontent.com/4975310/36114429-045eb676-0ffe-11e8-86cd-e180dc04b605.png)
+![7](https://user-images.githubusercontent.com/4975310/36114427-043f8e22-0ffe-11e8-9506-cf72f390af1b.png)
+![8](https://user-images.githubusercontent.com/4975310/36114426-04195d60-0ffe-11e8-8e1f-fcbf35ec38fd.png)
+
+## Requirements
+
+- Linux operating system (this plugin doesn't work on Windows, and I not sure about Mac).
+- [pydbus](https://github.com/LEW21/pydbus) (see [requirements](https://github.com/LEW21/pydbus#requirements)).
+- [wmctrl](https://en.wikipedia.org/wiki/Wmctrl) (optional, required only for `open` command)
+    - `sudo apt-get install wmctrl`
+    - `sudo dnf install wmctrl`
+
+## Install
+
+Install using [vim-plug](https://github.com/junegunn/vim-plug).
+Put this on your `init.vim`.
+
+```vim
+Plug 'stsewd/spotify.nvim', { 'do': ':UpdateRemotePlugins' }
+```
+
+## Usage
+
+- Start Spotify.
+- Call `:Spotify {option}`. Where option can be:
+  - `play/pause`
+  - `next`
+  - `prev`
+  - `play`
+  - `pause`
+  - `stop`
+  - `open`: focus Spotify window.
+  - `status`: show current song and player status.
+
+## Configuration
+
+### Show current song and player status after each command
+
+```vim
+g:spotify_show_status = 1
+```
+
+### Style of the symbols in the status
+
+Options can be:
+
+- `text`
+- `ascii` (default)
+- `emoji`
+
+```vim
+g:spotify_status_style = 'ascii'
+```
+
+### Format of the status
+
+The available variables are:
+
+- `status`: the current status (the style is decided by `g:spotify_status_style`).
+- `song`: the current song name.
+- `artists`: comma separated artists.
+- `decorator`: only visible when a song is playing.
+
+_Note_: this is a python format string.
+
+```vim
+g:spotify_status_format = ' {status} {song} - {artists} {decorator}'
+```
+
+### Wait time
+
+Time in milliseconds to wait to show the player status after each command.
+
+_This is needed, since Spotify could return the previous state, no the current one_.
+
+```vim
+g:spotify_wait_time = 0.2
+```
+
+## Mappings example
+
+```vim
+nnoremap <C-s>n :Spotify next<CR>
+nnoremap <C-s>p :Spotify prev<CR>
+nnoremap <C-s>s :Spotify play/pause<CR>
+nnoremap <C-s>o :Spotify open<CR>
+nnoremap <C-s>c :Spotify status<CR>
+```
+
+## References
+
+- https://specifications.freedesktop.org/mpris-spec/latest/Player_Interface.html
+- https://github.com/LEW21/pydbus/blob/master/doc/tutorial.rst
---
+++ examples/python/rplugin/python3/spotify_nvim.py
@@ -0,0 +1,195 @@
+import os
+import sys
+import time
+from subprocess import PIPE, Popen
+
+import neovim
+
+try:
+    from pydbus import SessionBus
+except ImportError:
+    # Try to import from python system packages
+    sys.path.insert(0, os.path.abspath('/usr/lib/python3.6/site-packages'))
+    sys.path.insert(0, os.path.abspath('/usr/lib64/python3.6/site-packages'))
+    from pydbus import SessionBus
+
+
+OPTIONS = [
+    ('play/pause', 'PlayPause'),
+    ('next', 'Next'),
+    ('prev', 'Previous'),
+    ('play', 'Play'),
+    ('pause', 'Pause'),
+    ('stop', 'Stop'),
+    ('open', '_open_spotify'),
+    ('status', '_show_current_status'),
+]
+OPTIONS_DICT = dict(OPTIONS)
+
+SYMBOLS_REPR = {
+    'playing': {
+        'text': '[playing]',
+        'ascii': '►',
+        'emoji': '▶️',
+    },
+    'paused': {
+        'text': '[paused]',
+        'ascii': '■',
+        'emoji': '⏸️',
+    },
+    'stopped': {
+        'text': '[stopped]',
+        'ascii': '■',
+        'emoji': '⏸️',
+    },
+    'music': {
+        'text': '',
+        'ascii': '♫♪',
+        'emoji': '🎶',
+    }
+}
+
+
+def setup_spotify(fun):
+    def wrapper(self, *args, **kwargs):
+        self.spotify = _get_spotify_proxy()
+        if not self.spotify:
+            self.error('Spotify is not running')
+            return
+        return fun(self, *args, **kwargs)
+    return wrapper
+
+
+def _get_spotify_proxy():
+    bus = SessionBus()
+    try:
+        proxy = bus.get(
+            'org.mpris.MediaPlayer2.spotify',
+            '/org/mpris/MediaPlayer2'
+        )
+        return proxy
+    except Exception:
+        return None
+
+
+def _get_spotify_pids():
+    command = ['pgrep', 'spotify']
+    with Popen(command, stdout=PIPE, stderr=PIPE) as proc:
+        output, error = proc.communicate()
+        return output.decode().split('\n')
+    return []
+
+
+def _get_window_id(pids):
+    command = [
+        'wmctrl',
+        '-l',  # list windows
+        '-p',  # show pids
+    ]
+    with Popen(command, stdout=PIPE, stderr=PIPE) as proc:
+        output, error = proc.communicate()
+        windows = output.decode().split('\n')
+        for window in windows:
+            window_id, workspace, pid, *_ = window.split()
+            if pid in pids:
+                return window_id
+    return None
+
+
+def _focus_window(window_id):
+    command = [
+        'wmctrl',
+        '-i',  # search by id
+        '-a',  # search
+        window_id,
+    ]
+    with Popen(command, stderr=PIPE) as proc:
+        output, error = proc.communicate()
+
+
+@neovim.plugin
+class SpotifyNvim:
+
+    def __init__(self, nvim):
+        self.nvim = nvim
+        self.spotify = None
+        self.show_status = self.nvim.vars.get('spotify_show_status', 1)
+        self.wait_time = self.nvim.vars.get('spotify_wait_time', 0.2)
+        self.status_style = self.nvim.vars.get('spotify_status_style', 'ascii')
+        self.status_format = self.nvim.vars.get(
+            'spotify_status_format',
+            ' {status} {song} - {artists} {decorator}'
+        )
+
+    def _show_current_status(self):
+        data = self.spotify.Metadata
+        song = data.get('xesam:title', 'No Title')
+        artists = data.get('xesam:artist', ['Unknow'])
+        status = self.spotify.PlaybackStatus
+        self._show_status(
+            status=status, song=song, artists=artists
+        )
+
+    def _show_status(self, *, status, song, artists):
+        decorator = ''
+        if status.lower() == 'playing':
+            decorator = self._get_symbol_repr('music')
+        status_format = self.status_format + '\n'
+        self.nvim.out_write(status_format.format(
+            status=self._get_symbol_repr(status),
+            song=song,
+            artists=', '.join(artists),
+            decorator=decorator
+        ))
+
+    def _get_symbol_repr(self, symbol):
+        symbol = symbol.lower()
+        repr_ = (
+            SYMBOLS_REPR
+            .get(symbol, {})
+            .get(self.status_style, '')
+        )
+        return repr_
+
+    def _open_spotify(self):
+        spotify_pids = _get_spotify_pids()
+        window_id = _get_window_id(spotify_pids)
+        if window_id:
+            _focus_window(window_id)
+        else:
+            self.error('Spotify is not running')
+
+    def error(self, msg):
+        self.nvim.err_write('[spotify] {}\n'.format(msg))
+
+    def print(self, msg):
+        self.nvim.out_write('[spotify] {}\n'.format(msg))
+
+    @neovim.command(
+        'Spotify', nargs=1, complete='customlist,SpotifyCompletions')
+    @setup_spotify
+    def spotify_command(self, args):
+        attr = OPTIONS_DICT.get(args[0])
+        if attr:
+            if attr.startswith('_'):
+                method = getattr(self, attr)
+            else:
+                method = getattr(self.spotify, attr)
+
+            method()
+            if self.show_status and not attr.startswith('_'):
+                # We need to wait to the previous
+                # command to get executed
+                time.sleep(self.wait_time)
+                self._show_current_status()
+        else:
+            self.error('Invalid option')
+
+    @neovim.function('SpotifyCompletions', sync=True)
+    def spotify_completions(self, args):
+        arglead, cmdline, cursorpos, *_ = args
+        return [
+            option
+            for option, action in OPTIONS
+            if option.lower().startswith(arglead.lower())
+        ]
